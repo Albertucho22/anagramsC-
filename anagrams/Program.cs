9507d9588b6e68df9fe147dbd318d3f4e894fa6d
lDiego257 <lDiego257>
2020-12-17 21:16:32 -0400

    Diego Commit

--- anagrams/Program.cs
+++ anagrams/Program.cs
@@ -15,15 +15,16 @@ namespace anagrams
             var path = @"C:\Users\ALBERTO\Documents\Trimestre 10\Tendencias en desarrollo de aplicaciones\Semana 7 idk\Text.txt";
             var outPath = @"C:\Users\ALBERTO\Documents\Trimestre 10\Tendencias en desarrollo de aplicaciones\Semana 7 idk\Output.txt";
             
-            p.overWriteInFile(outPath); //create an empty txt file
+            //p.overWriteInFile(outPath); //create an empty txt file
 
             //WE CAN SELECT ONE OR THE TWO OPTIONS
-            
+            int actual = p.findAllAnagramsInFile(@"../input.txt",@"../Output.txt");
+            int expected = 0;
             //1
-            p.findAllAnagramsInFile(path, outPath); //find 20683 sets of anagrams and write them in a file "Output.txt"
+            //p.findAllAnagramsInFile(path, outPath); //find 20683 sets of anagrams and write them in a file "Output.txt"
             
             //2
-            Console.WriteLine("\n" + p.findAnSpecificAnagramsInFile(path, "crepitus")); //Find anagrams of an specific word
+            //Console.WriteLine("\n" + p.findAnSpecificAnagramsInFile(path, "crepitus")); //Find anagrams of an specific word
         }
         public string writeInFile(string outPath, string g)
         {
@@ -35,18 +36,19 @@ namespace anagrams
             return g;
         }
 
-        public void findAllAnagramsInFile(string path, string outPath)
+        public int findAllAnagramsInFile(string path, string outPath)
         {
             Program p = new Program();
 
-            File.ReadAllLines(path)
-            .GroupBy(w => new string(w.OrderBy(c => c).ToArray()))
-            //.GroupBy(new string(w.OrderBy(c => c).ToArray()))
-            .Where(g => g.Count() > 1)
-            //.ToList().ForEach(g => Console.WriteLine(String.Join(" ", g)));
-            .ToList().ForEach(g => p.writeInFile(outPath, (String.Join(" ", g))));
+            List<IGrouping<string, string>> lista = File.ReadAllLines(path)
+                        .GroupBy(w => new string(w.OrderBy(c => c).ToArray()))
+                        .Where(g => g.Count() > 1).ToList()
+                        .ToList();
+            foreach(var a in lista){
+                p.writeInFile(outPath, (String.Join(" ",a)));
+            }
+            return lista.Count;
         }
-
         public bool findAnSpecificAnagramsInFile(string path, string g)
         {
             Program p = new Program();
